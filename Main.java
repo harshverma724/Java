1eb31d4a9e85a15fb27e85a26f43265299a9151c
Harsh <harshvermajpr1@gmail.com>
2024-03-05 00:13:46 +0530

    Add files via upload
    Make a 2D array 
    put values in it 
    print that array

---
+++ Main.java
@@ -0,0 +1,48 @@
+//Get the bytes used by the 2D array.
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+        int element;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of rows here: ");
+        int r = sc.nextInt();
+        System.out.print("Enter the no. of columns here: ");
+        int c = sc.nextInt();
+
+
+        //Forming the array
+        int[][] array = new int[r][c];
+
+
+        //getting the size of the array in bytes(due to int datatype)
+        System.out.println("Memory Utilised by this 2D array is: " + ((r * c) * 4) + " bytes.");
+
+
+        //assigning the values to the array
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print("Enter the " + i + "," + j + " element: ");
+                element = sc.nextInt();
+                array[i][j] = element;
+                System.out.println(" ");
+
+            }
+        }
+
+
+        // printing the array elements.
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(array[i][j]);
+                System.out.print(" | ");
+            }
+            System.out.println(" ");
+
+            }
+
+
+        }
+    }
